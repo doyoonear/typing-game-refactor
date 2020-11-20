0e194da11a469615f6ed772a5d853b3fa7801e8a
Doyoonear <doyoonleee@gmail.com>
2020-11-20 22:37:40 +0900

    Add: 타이머 기능, 확인하고 다음 단어 표시 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import Style from '../index.css';
+global.fetch = require("node-fetch");
 import { timerOn, nextTimer } from '../src/timer';
+import "regenerator-runtime/runtime";
--- src/timer.js
+++ src/timer.js
@@ -5,150 +5,109 @@ const START_BTN = document.querySelector('.start');
 const TEMP_BTN = document.querySelector('.temp');
 const WORD_CON = document.querySelector('.wordContainer');
 const docFrag = document.createDocumentFragment(); // fragment 생성
-const wordList = [];
-
-START_BTN.addEventListener('click', timerOn, false);
-INPUT.addEventListener('keyup', checkVal, false);
-
-window.onload = function() {
-fetch('https://my-json-server.typicode.com/kakaopay-fe/resources/words')
-  .then(res => res.json())
-  .then(res => wordList.concat(res))
-  .then(res => console.log('wordList', wordList))
-  .catch(err => console.log(err.message));
-}
-
-// 받아온 data 중에 text는 word 로 주고, sec는 TIMER에 준다.
-// 랜덤 숫자 배열 길이가 10이 되면, clearInterval
+let wordList = [];
+let randomArr = [];
+let randomNum = Math.floor(Math.random() * 12); 
 let intervalId = null;
 
-export function timerOn() {
-  intervalId = window.setInterval(() => {
-    TIMER.textContent -= 1;
-  }, 1000);
-  console.log(TIMER.textContent);
-  return intervalId;
-}
 
-// export function nextTimer() {
-//     TIMER.textContent = 10;
-//     clearInterval(timerOn());
-// }
-
-// 랜덤 숫자 배열이 10이 되면 자동으로 게임 종료 후 완료 페이지로 이동 시킨다. 
-export function stopGame() {
-  if(TIMER.textContent === -2) {
-    TIMER.textContent = 10;
-    clearInterval(intervalId);
-  }
-  console.log('stopGame');
-}
-
-// 사용자가 중지 버튼을 클릭했을 때 게임 리셋, 랜덤배열 초기화.
-export function resetGame() {
-  console.log('resetGame');
-}
-
-// 0 에서 12의 랜덤 숫자를 인덱스로 사용, 한번 사용한 단어는 제외한다. 
-// 사용한 인덱스를 배열에 저장, 새로만든 랜덤 숫자가 배열에 있으면 다른 랜덤 숫자 사용
-export function getNum() {
-  let randomNum = Math.floor(Math.random() * (12 - 0)); 
-  console.log('random',randomNum); 
-  WORD.textContent = wordList[randomNum].text;
-  TIMER.textContent = wordList[randomNum].text;
-  return WORD;
-}
-
-// 엔터키 입력했을때 발동. 입력창 INPUT의 입력한값 제시어와 확인하여 맞으면 
-// INPUT value 가 같으면 INPUT을 clear하고, 단어리스트의 다음인덱스를 가져온다.
-// 같으면 점수 count를 올린다. 
-export function checkVal(e) {
-  if(e.keyCode === 13) {
-    if(INPUT.value === WORD.textContent) {
-      INPUT.value = '';
-      getNum();
-    }
-    e.stopPropagation;
-    return;
-  }
-  
-}
-
-const INPUT = document.querySelector('#write');
-const WORD = document.querySelector('.word');
-const TIMER = document.querySelector('.time');
-const START_BTN = document.querySelector('.start');
-const TEMP_BTN = document.querySelector('.temp');
-const WORD_CON = document.querySelector('.wordContainer');
-const docFrag = document.createDocumentFragment(); // fragment 생성
-const wordList = [];
-
-START_BTN.addEventListener('click', timerOn, false);
-START_BTN.addEventListener('click', stopGame, false);
+window.addEventListener('load', getData, false);
 INPUT.addEventListener('keyup', checkVal, false);
 
-window.onload = function() {
-fetch('https://my-json-server.typicode.com/kakaopay-fe/resources/words')
-  .then(res => res.json())
-  .then(res => wordList.concat(res))
-  .then(res => console.log('wordList', wordList))
-  .catch(err => console.log(err.message));
+async function getData() {
+  let url = 'https://my-json-server.typicode.com/kakaopay-fe/resources/words';
+  let response = await fetch(url);
+  if (response.ok) { 
+    wordList = await response.json();
+    console.log('word', wordList);
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
 }
 
-// 받아온 data 중에 text는 word 로 주고, sec는 TIMER에 준다.
-// 랜덤 숫자 배열 길이가 10이 되면, clearInterval
-let intervalId = null;
+START_BTN.onclick = function toggle() {
+  if(START_BTN.textContent === '시작') {
+    return startGame();
+  }
+  if(START_BTN.textContent === '초기화') {
+    return resetGame();
+  }
+}
 
-export function timerOn() {
-  console.log(START_BTN.textContent);
+// 시작 버튼을 눌러 게임을 시작했을 때 함수. second -1씩
+export async function startGame() { 
+  await getData();
+  WORD.textContent = wordList[randomNum].text;
+  TIMER.textContent = wordList[randomNum].second;
+  START_BTN.textContent = '초기화';
   intervalId = window.setInterval(() => {
     TIMER.textContent -= 1;
-  }, 1000);
-  console.log(TIMER.textContent);
-  return intervalId;
-}
-
-// export function nextTimer() {
-//     TIMER.textContent = 10;
-//     clearInterval(timerOn());
-// }
-
-// 랜덤 숫자 배열이 10이 되면 자동으로 게임 종료 후 완료 페이지로 이동 시킨다. 
-export function stopGame() {
-  console.log('stopGame');
-  console.log(e.target);
-  if(TIMER.textContent === 0) {
-    TIMER.textContent = 10;
-    clearInterval(intervalId);
-  }
-}
-
-// 사용자가 중지 버튼을 클릭했을 때 게임 리셋, 랜덤배열 초기화.
-export function resetGame() {
-  console.log('resetGame');
-}
-
-// 0 에서 12의 랜덤 숫자를 인덱스로 사용, 한번 사용한 단어는 제외한다. 
-// 사용한 인덱스를 배열에 저장, 새로만든 랜덤 숫자가 배열에 있으면 다른 랜덤 숫자 사용
-export function getNum() {
-  let randomNum = Math.floor(Math.random() * (12 - 0)); 
-  console.log('random',randomNum); 
-  WORD.textContent = wordList[randomNum].text;
-  TIMER.textContent = wordList[randomNum].text;
-  return WORD;
-}
-
-// 엔터키 입력했을때 발동. 입력창 INPUT의 입력한값 제시어와 확인하여 맞으면 
-// INPUT value 가 같으면 INPUT을 clear하고, 단어리스트의 다음인덱스를 가져온다.
-// 같으면 점수 count를 올린다. 
-export function checkVal(e) {
-  if(e.keyCode === 13) {
-    if(INPUT.value === WORD.textContent) {
-      INPUT.value = '';
-      getNum();
+    if(Number(TIMER.textContent) === 0) {
+      return nextWord();
     }
-    e.stopPropagation;
-    return;
-  }
-  
+  }, 1000);
 }
+
+// 버튼을 눌러 게임을 중단했을 때 실행되는 함수. 랜덤배열 초기화.
+export function resetGame() {
+  clearInterval(intervalId);
+  START_BTN.textContent = '시작';
+  WORD.textContent = '문제 단어';
+  TIMER.textContent = 10;
+  randomArr = [];
+}
+
+
+// 0 에서 12의 랜덤 숫자를 wordList의 인덱스로 사용.
+// 사용한 인덱스를 배열에 저장, 새로만든 랜덤 숫자가 배열에 있으면,
+// 새로운 랜덤 숫자를 만들고, 중복되지 않는 숫자를 랜덤 숫자에 넣어서 
+export function getNum() {
+  randomNum = Math.floor(Math.random() * 12); 
+  if(randomArr.includes(randomNum)) {
+    for(let i = 0; i < wordList.length; i++) {
+      if(i !== randomNum){ 
+        randomNum = i; 
+        return randomNum;
+      }
+    }
+  }
+  else {
+    return randomNum;
+  }
+}
+
+// 단어 10개 채웠는지 체크, 엔터쳤을 때 일치하는지 체크
+export function checkVal(e) {
+  let match = (INPUT.value.toLowerCase() === WORD.textContent.toLowerCase());
+  console.log(match);
+  if(e.keyCode === 13 && match) {
+    nextWord();
+  }
+  e.stopPropagation;
+  return null;
+}
+
+// input창을 비우고, 글자와 초를 새로운걸로 바꾼다. 사용한 인덱스는 arr에 담는다. 
+export async function nextWord() {
+  await getNum();
+  if(randomArr.length <= 10) {
+    INPUT.value = '';
+    WORD.textContent = wordList[randomNum].text;
+    TIMER.textContent = wordList[randomNum].second;
+    return randomArr.push(randomNum);
+  }
+  else {
+    return endGame();
+  }
+}
+
+// 10개를 다 풀었을 때, 게임 종료 후 완료 페이지로 이동
+export function endGame() {
+  clearInterval(intervalId);
+  console.log('endGame');
+}
+
+
+
+
+
